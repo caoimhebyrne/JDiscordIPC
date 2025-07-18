6ec5967f673e4ef0222e9a25cd559840d8d79cd0
Caoimhe <71222289+caoimhebyrne@users.noreply.github.com>
2025-07-18 21:34:11 +0100

    chore: move packet reading to its own function

--- src/main/java/dev/caoimhe/jdiscordipc/JDiscordIPC.java
+++ src/main/java/dev/caoimhe/jdiscordipc/JDiscordIPC.java
@@ -44,6 +44,7 @@ public class JDiscordIPC {
 
     /**
      * Connects to the running Discord application through the {@link SystemSocket} provided during initialization.
+     * This method will block until the connection is initiated, but not until it is ready.
      */
     public void connect() throws DiscordClientUnavailableException {
         final Path discordIpcPath = this.getIpcFilePath();
@@ -55,21 +56,27 @@ public class JDiscordIPC {
         }
 
         // Now that we've connected, we can start a background task to start consuming messages from Discord.
-        this.executorService.execute(() -> {
-            while (this.systemSocket.isConnected()) {
-                try {
-                    final Packet packet = this.packetCodec.read();
-                    System.out.println(packet);
-                } catch (final Exception e) {
-                    System.err.println("Failed to read packet: " + e);
-                }
-            }
-        });
+        this.executorService.execute(this::readPackets);
 
+        // To mark the connection as ready, we must send a handshake to the client containing our client ID.
         try {
             this.packetCodec.write(new HandshakePacket(this.clientId));
         } catch (final IOException e) {
             System.err.println("Failed to write packet to socket: " + e);
+        }
+    }
+
+    /**
+     * Responsible for reading packets from the Discord client and ensuring they get handled.
+     */
+    private void readPackets() {
+        while (this.systemSocket.isConnected()) {
+            try {
+                final Packet packet = this.packetCodec.read();
+                System.out.println(packet);
+            } catch (final Exception e) {
+                System.err.println("Failed to read packet: " + e);
+            }
         }
     }
 
