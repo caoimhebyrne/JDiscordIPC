500054c97915aeb74399723118336d73a8290d74
Caoimhe Byrne <caoimhebyrne06@gmail.com>
2025-07-18 21:45:27 +0100

    feat: add the ability to register event listeners

--- example/src/main/java/Main.java
+++ example/src/main/java/Main.java
@@ -1,6 +1,8 @@
 import dev.caoimhe.jdiscordipc.JDiscordIPC;
 import dev.caoimhe.jdiscordipc.builder.JDiscordIPCBuilder;
+import dev.caoimhe.jdiscordipc.event.DiscordEventListener;
 import dev.caoimhe.jdiscordipc.exception.DiscordClientUnavailableException;
+import dev.caoimhe.jdiscordipc.model.event.ReadyEvent;
 import dev.caoimhe.jdiscordipc.modern.core.ModernSystemSocketFactory;
 
 public class Main {
@@ -8,6 +10,13 @@ public class Main {
         final JDiscordIPC jDiscordIPC = JDiscordIPCBuilder.of(945428344806183003L)
             .systemSocketFactory(new ModernSystemSocketFactory())
             .build();
+
+        jDiscordIPC.registerEventListener(new DiscordEventListener() {
+            @Override
+            public void onReadyEvent(ReadyEvent event) {
+                System.out.println("Ready!");
+            }
+        });
 
         try {
             jDiscordIPC.connect();
