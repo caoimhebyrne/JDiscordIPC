800e01f6af6a22c24031c1d7e0a0b9d26208896f
Caoimhe <71222289+caoimhebyrne@users.noreply.github.com>
2025-07-19 10:06:05 +0100

    feat: allow activity updates to be queued until the client is ready

--- example/src/main/java/Main.java
+++ example/src/main/java/Main.java
@@ -1,10 +1,8 @@
 import dev.caoimhe.jdiscordipc.JDiscordIPC;
 import dev.caoimhe.jdiscordipc.builder.JDiscordIPCBuilder;
-import dev.caoimhe.jdiscordipc.event.DiscordEventListener;
 import dev.caoimhe.jdiscordipc.exception.JDiscordIPCException;
 import dev.caoimhe.jdiscordipc.model.activity.Activity;
 import dev.caoimhe.jdiscordipc.model.activity.party.ActivityPartyPrivacy;
-import dev.caoimhe.jdiscordipc.model.event.ReadyEvent;
 import dev.caoimhe.jdiscordipc.modern.core.ModernSystemSocketFactory;
 
 public class Main {
@@ -13,24 +11,24 @@ public class Main {
             .systemSocketFactory(new ModernSystemSocketFactory())
             .build();
 
-        jDiscordIPC.registerEventListener(new DiscordEventListener() {
-            @Override
-            public void onReadyEvent(final ReadyEvent event) {
-                final Activity activity = Activity.builder()
-                    // The activity details is the state of what the user is doing in the activity.
-                    .details("Selecting a game mode")
-                    // The activity state is the state of the party in the activity.
-                    .state("In lobby")
-                    .party("party-1", 2, (builder) -> {
-                        builder.maximumSize(10);
-                        builder.privacy(ActivityPartyPrivacy.PUBLIC);
-                    })
-                    .build();
+        final Activity activity = Activity.builder()
+            // The activity details is the state of what the user is doing in the activity.
+            .details("Selecting a game mode")
+            // The activity state is the state of the party in the activity.
+            .state("In lobby")
+            .party("party-1", 2, (builder) -> {
+                builder.maximumSize(10);
+                builder.privacy(ActivityPartyPrivacy.PUBLIC);
+            })
+            .build();
 
-                jDiscordIPC.updateActivity(activity);
-            }
-        });
+        // If an activity is set before JDiscordIPC is connected, it'll queue the activity to be set once a ready
+        // event is received from Discord.
+        jDiscordIPC.updateActivity(activity);
 
+        // This will attempt to initialize the connection between JDiscordIPC and the Discord client.
+        // This will block for a short amount of time, but not for too long. If you wish to know when the Discord client
+        // is *completely ready*, check out the `DiscordEventListener` and `JDiscordIPC#registerEventListener`.
         try {
             jDiscordIPC.connect();
         } catch (final JDiscordIPCException.DiscordClientUnavailableException e) {
