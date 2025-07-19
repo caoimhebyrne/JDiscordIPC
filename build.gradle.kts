2fdb2b6aa470574ec3ad1905a87d136a6e39655e
Caoimhe <71222289+caoimhebyrne@users.noreply.github.com>
2025-07-19 21:18:22 +0100

    build: use api for jspecify annotations

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,8 +11,8 @@ repositories {
 }
 
 dependencies {
+    api("org.jspecify:jspecify:1.0.0")
     implementation("com.fasterxml.jackson.core:jackson-databind:2.17.2")
-    implementation("org.jspecify:jspecify:1.0.0")
 
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
