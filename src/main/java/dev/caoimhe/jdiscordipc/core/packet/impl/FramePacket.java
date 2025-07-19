512f63c654ce273f6d2bd84301c3383aba883fc9
Caoimhe Byrne <caoimhebyrne06@gmail.com>
2025-07-19 08:39:39 +0100

    feat: separate incoming and outgoing frame packets

--- src/main/java/dev/caoimhe/jdiscordipc/core/packet/impl/FramePacket.java
+++ src/main/java/dev/caoimhe/jdiscordipc/core/packet/impl/FramePacket.java
@@ -1,28 +1,25 @@
 package dev.caoimhe.jdiscordipc.core.packet.impl;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.annotation.JsonSubTypes;
-import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import dev.caoimhe.jdiscordipc.core.packet.Packet;
 import dev.caoimhe.jdiscordipc.core.packet.PacketOpcode;
-import dev.caoimhe.jdiscordipc.core.packet.impl.frame.DispatchEventPacket;
 import org.jspecify.annotations.Nullable;
 
 /**
  * A base class for all frame packets to implement, most packets within the protocol are a form of frame packet.
  */
-@JsonTypeInfo(use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.EXISTING_PROPERTY, property = "cmd")
-@JsonSubTypes(
-    @JsonSubTypes.Type(value = DispatchEventPacket.class, name = "DISPATCH")
-)
-public abstract class FramePacket<D> implements Packet {
-    /** The command for this frame packet, used as an indicator for the actual data within the packet. */
+public abstract class FramePacket implements Packet {
+    /**
+     * The command for this frame packet, used as an indicator for the actual data within the packet.
+     */
     @JsonProperty("cmd")
-    private String command;
+    protected String command;
 
-    /** The unique ID for this frame packet. */
+    /**
+     * The unique ID for this frame packet.
+     */
     @JsonProperty("nonce")
-    private @Nullable String uniqueId;
+    protected @Nullable String uniqueId;
 
     /**
      * The command for this frame packet, used as an indicator for the actual data within the packet.
@@ -30,11 +27,6 @@ public abstract class FramePacket<D> implements Packet {
     public String command() {
         return this.command;
     }
-
-    /**
-     * The data within this frame packet.
-     */
-    public abstract D data();
 
     /**
      * The unique ID for this frame packet.
